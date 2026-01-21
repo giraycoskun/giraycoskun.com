8ab20fb6b45625f77acbf504ee897be9651066a4
giraycoskun <giraycoskun.dev@gmail.com>
2026-01-21 15:00:59 +0100

    feat: :sparkles: add sitemap integration and create robots.txt for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,5 +20,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
